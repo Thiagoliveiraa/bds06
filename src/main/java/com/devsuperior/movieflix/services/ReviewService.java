b32c6f0e171f0531177d92744a9acffc67c08f32
Thiago Oliveira <thiagooliveiraaa@hotmail.com>
2022-05-15 20:12:02 -0300

    create ReviewService

---
+++ src/main/java/com/devsuperior/movieflix/services/ReviewService.java
@@ -0,0 +1,35 @@
+package com.devsuperior.movieflix.services;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.movieflix.dto.ReviewDTO;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.entities.Review;
+import com.devsuperior.movieflix.repositories.ReviewRepository;
+
+@Service
+public class ReviewService {
+
+	@Autowired
+	private ReviewRepository repository;
+	
+	@Autowired
+	private AuthService authService;
+	
+	@Transactional
+	public @Valid ReviewDTO insert( ReviewDTO dto) {
+		Review entity = new Review();
+		Movie movie = new Movie();
+		entity.setText(dto.getText());
+		movie.setId(dto.getMovieId());
+		entity.setMovie(movie);
+		entity.setUser(authService.authenticated());
+		entity = repository.save(entity);
+		return new ReviewDTO(entity);		
+	}
+
+}
